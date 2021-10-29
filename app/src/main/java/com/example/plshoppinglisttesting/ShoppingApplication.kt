efc24271a6ae78e93bfe836343e736b16e4237bc
Brian Donnoe <dunow33@hotmail.com>
2021-10-29 01:02:30 -0400

    got some database stuff set up

---
+++ app/src/main/java/com/example/plshoppinglisttesting/ShoppingApplication.kt
@@ -0,0 +1,7 @@
+package com.example.plshoppinglisttesting
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShoppingApplication : Application()
---
+++ app/src/main/java/com/example/plshoppinglisttesting/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.example.plshoppinglisttesting.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.plshoppinglisttesting.data.local.ShoppingItemDatabase
+import com.example.plshoppinglisttesting.data.remote.PixabayAPI
+import com.example.plshoppinglisttesting.other.Constants.BASE_URL
+import com.example.plshoppinglisttesting.other.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideShoppingItemDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, ShoppingItemDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideShoppingDao(
+        database: ShoppingItemDatabase
+    ) = database.shoppingDao()
+
+    @Singleton
+    @Provides
+    fun providePixabayApi(): PixabayAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PixabayAPI::class.java)
+    }
+}
