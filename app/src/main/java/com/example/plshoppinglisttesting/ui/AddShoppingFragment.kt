a7236496c1fbf12cee20f34e64d239478df2c954
Brian Donnoe <dunow33@hotmail.com>
2021-10-30 18:02:11 -0400

    finished video 13

--- app/src/main/java/com/example/plshoppinglisttesting/ui/AddShoppingFragment.kt
+++ app/src/main/java/com/example/plshoppinglisttesting/ui/AddShoppingFragment.kt
@@ -2,9 +2,14 @@ package com.example.plshoppinglisttesting.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.plshoppinglisttesting.R
+import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
+//import kotlinx.android.synthetic.main.item_image.*
+//import kotlinx.android.synthetic.main.item_image.ivShoppingImage
 
 class AddShoppingFragment : Fragment(R.layout.fragment_add_shopping_item) {
 
@@ -13,5 +18,19 @@ class AddShoppingFragment : Fragment(R.layout.fragment_add_shopping_item) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+
+        ivShoppingImage.setOnClickListener {
+            findNavController().navigate(
+                AddShoppingFragmentDirections.actionAddShoppingFragmentToImagePickFragment()
+            )
+        }
+
+        val callback = object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                viewModel.setCurImageUrl("")
+                findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
     }
 }
