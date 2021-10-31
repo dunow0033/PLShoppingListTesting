0c265cb303aa18a75f74ac811e8d44513ce9d819
Brian Donnoe <dunow33@hotmail.com>
2021-10-30 20:49:51 -0400

    finished video 14

--- app/src/main/java/com/example/plshoppinglisttesting/di/AppModule.kt
+++ app/src/main/java/com/example/plshoppinglisttesting/di/AppModule.kt
@@ -2,6 +2,9 @@ package com.example.plshoppinglisttesting.di
 
 import android.content.Context
 import androidx.room.Room
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.plshoppinglisttesting.R
 import com.example.plshoppinglisttesting.data.local.ShoppingDao
 import com.example.plshoppinglisttesting.data.local.ShoppingItemDatabase
 import com.example.plshoppinglisttesting.data.remote.PixabayAPI
@@ -34,6 +37,16 @@ object AppModule {
         dao: ShoppingDao,
         api: PixabayAPI
     ) = DefaultShoppingRepository(dao, api) as ShoppingRepository
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_image)
+            .error(R.drawable.ic_image)
+    )
 
     @Singleton
     @Provides
---
+++ app/src/main/java/com/example/plshoppinglisttesting/ui/ShoppingFragmentFactory.kt
@@ -0,0 +1,18 @@
+package com.example.plshoppinglisttesting.ui
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import com.example.plshoppinglisttesting.adapters.ImageAdapter
+import javax.inject.Inject
+
+class ShoppingFragmentFactory @Inject constructor(
+    private val imageAdapter: ImageAdapter
+) : FragmentFactory() {
+
+    override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+        return when(className) {
+            ImagePickFragment::class.java.name -> ImagePickFragment(imageAdapter)
+            else -> super.instantiate(classLoader, className)
+        }
+    }
+}
